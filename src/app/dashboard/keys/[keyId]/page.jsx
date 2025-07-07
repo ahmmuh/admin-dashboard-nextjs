774d2f8454201b6e31042348f784d8543dfe55aa
ahmmuh <mukhtar1100@hotmail.com>
2025-07-07 02:52:57 +0200

    ok

--- src/app/dashboard/keys/[keyId]/page.jsx
+++ src/app/dashboard/keys/[keyId]/page.jsx
@@ -4,6 +4,7 @@ import { deleteKey, getKeyByID, updateKey } from "@/backend/keyAPI";
 import { displayErrorMessage } from "@/helper/toastAPI";
 import { useParams, useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { HiOutlinePencilAlt, HiOutlineTrash } from "react-icons/hi";
 
 function EditKey() {
   const [key, setKey] = useState({
@@ -134,14 +135,16 @@ function EditKey() {
 
           <div className="flex justify-start  p-4 ">
             <button
-              onClick={deleteKeyHandler}
-              className="bg-red-400 text-white text-center px-4 py-2 w-64 hover:bg-red-500">
-              Delete
+              onClick={changeKeyHandler}
+              className="w-1/3 flex items-center gap-2 px-4 py-2 bg-indigo-100 text-indigo-800 border border-indigo-300 rounded-md shadow-sm hover:bg-indigo-200 transition">
+              <HiOutlinePencilAlt className="w-5 h-5" />
+              Spara
             </button>
             <button
-              onClick={changeKeyHandler}
-              className="bg-green-400  text-white text-center px-4 py-2 w-64 ml-4 hover:bg-green-500">
-              Update
+              onClick={deleteKeyHandler}
+              className="w-1/3 ml-3 flex items-center gap-2 px-4 py-2 bg-red-100 text-red-700 border border-red-300 rounded-md shadow-sm hover:bg-red-200 transition">
+              <HiOutlineTrash className="w-5 h-5" />
+              Ta bort
             </button>
             {/* <button
               onClick={changeKeyHandler}
